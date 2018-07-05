180a1b9348ec36feaa1fd18c16c96be4d7b234c9
Alberto Romeu <alrocar@cartodb.com>
2018-07-05 08:05:47 +0200

    refactor

--- jurgol.py
+++ jurgol.py
@@ -1,6 +1,6 @@
 from io import StringIO
 
-# import ipdb
+import ipdb
 
 import csv
 import json
@@ -15,25 +15,57 @@ USERNAME = "aromeu"
 USR_BASE_URL = "https://{user}.carto.com/".format(user=USERNAME)
 auth_client = APIKeyAuthClient(api_key=API_KEY, base_url=USR_BASE_URL)
 
-PATH = os.getcwd()
-
 STADIUMS_FILE_NAME = "stadiums.csv"
-STADIUMS_PATH = "{path}/{filename}".format(path = PATH, filename = STADIUMS_FILE_NAME)
+TVCHANNELS_FILE_NAME = "tvchannels.csv"
+TEAMS_FILE_NAME = "teams.csv"
+MATCHES_FILE_NAME = "matches.csv"
 
 data_url = 'https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json'
 response = urllib.request.urlopen(data_url)
 data = response.read().decode('utf-8')
 data_json = json.load(StringIO(data))
 
-with open(STADIUMS_FILE_NAME, 'w', newline='') as csvfile:
-    fieldnames = ['id', 'name', 'city', 'lat', 'lng', 'image']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+def write_csv(output_filename, fieldnames, obj_array):
+    with open(output_filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    writer.writeheader()
+        writer.writeheader()
 
-    stadiums = data_json['stadiums']
-    for stadium in stadiums:
-        writer.writerow(stadium)
+        for obj in obj_array:
+            yield(obj)
+            writer.writerow(obj)
 
-dataset_manager = DatasetManager(auth_client)
-dataset = dataset_manager.create(STADIUMS_PATH)
+def global_path(filename):
+    return "{path}/{filename}".format(path = os.getcwd(), filename = filename)
+
+for obj in write_csv(STADIUMS_FILE_NAME, ['id', 'name', 'city', 'lat', 'lng', 'image'], data_json['stadiums']):
+    print(obj)
+
+for obj in write_csv(TVCHANNELS_FILE_NAME, ['id', 'name', 'icon', 'country', 'iso2', 'lang'], data_json['tvchannels']):
+    obj['lang'] = ' '.join(map(str, obj['lang'])).strip()
+
+for obj in write_csv(TEAMS_FILE_NAME, ['id', 'name', 'fifaCode', 'iso2', 'flag', 'emoji', 'emojiString'], data_json['teams']):
+    print(obj)
+
+matches = []
+for group in data_json['groups']:
+    # ipdb.set_trace(context=50)
+    matches = matches + data_json['groups'][group]['matches']
+
+for knockout in data_json['knockout']:
+    matches = matches + data_json['knockout'][knockout]['matches']
+
+ipdb.set_trace(context=50)
+fieldnames = ['name', 'type', 'home_team', 'away_team', 'home_result', 'away_result', 'date', 'stadium', 'channels', 'finished']
+for obj in write_csv(MATCHES_FILE_NAME, fieldnames, matches):
+    for key in obj:
+        if key not in fieldnames:
+            obj
+    obj['channels'] = ' ' #.join(map(str, obj['channels'])).strip()
+
+
+# dataset_manager = DatasetManager(auth_client)
+# dataset_manager.create(global_path(STADIUMS_FILE_NAME))
+# dataset_manager.create(global_path(TVCHANNELS_FILE_NAME))
+# dataset_manager.create(global_path(TEAMS_FILE_NAME))
+# dataset_manager.create(global_path(MATCHES_FILE_NAME))
